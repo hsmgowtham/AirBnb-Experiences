90103dd919ada4e70af48cef68bde52c04fe304f
hsmgowtham <hsmgowtham@gmail.com>
2024-05-22 08:14:23 -0400

    Add hero Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Main from './components/Main';
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+
 import './App.css';
 
 function App() {
   return (
     <div className="conatiner">
+      <NavBar />
+      <Hero />
       <Main />
     </div>
   );
